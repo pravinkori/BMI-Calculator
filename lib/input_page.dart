c8c982869e85dea227551c7da1a1b4fe8fa3dc7a
Pravin <pravinkori@protonmail.ch>
2022-12-10 19:35:34 +0530

    Custom resuable widget to display gender on cards.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const activeCardColour = Color(0xFF4C5065);
+const bottomContainerHeight = 80.0;
+const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -11,6 +18,8 @@ class _InputPageState extends State<InputPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BMI Calculator'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0A0E21),
       ),
       body: Column(
         children: [
@@ -19,12 +28,20 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: Card(
-                    colour: Color(0xFF4C5065),
+                    colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: "MALE",
+                    ),
                   ),
                 ),
                 Expanded(
                   child: Card(
-                    colour: Color(0xFF4C5065),
+                    colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: "FEMALE",
+                    ),
                   ),
                 ),
               ],
@@ -32,7 +49,7 @@ class _InputPageState extends State<InputPage> {
           ),
           Expanded(
             child: Card(
-              colour: Color(0xFF4C5065),
+              colour: activeCardColour,
             ),
           ),
           Expanded(
@@ -40,16 +57,22 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: Card(
-                    colour: Color(0xFF4C5065),
+                    colour: activeCardColour,
                   ),
                 ),
                 Expanded(
                   child: Card(
-                    colour: Color(0xFF4C5065),
+                    colour: activeCardColour,
                   ),
                 ),
               ],
             ),
+          ),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
           ),
         ],
       ),
@@ -57,14 +80,47 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  IconContent({required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class Card extends StatelessWidget {
-  Card({required this.colour});
+  Card({required this.colour, this.cardChild});
+
+  final Widget? cardChild;
   final Color colour;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(20.0),
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,17 @@ import 'package:flutter/material.dart';
 import 'input_page.dart';
 
 void main() {
-  runApp(const BMICalculator());
+  runApp(BMICalculator());
 }
 
 class BMICalculator extends StatelessWidget {
-  const BMICalculator({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData().copyWith(
-        primaryColor: const Color(0xFF04040c),
-        scaffoldBackgroundColor: const Color(0xFF04040c),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          secondary: const Color(0xFFfd0225),
-        ),
-        textTheme: const TextTheme(
-          bodyText1: TextStyle(color: Colors.white),
-        ),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
     );
