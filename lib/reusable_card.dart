c78dadda437ce52820fbedaa85d227e5b2dadb3f
Pravin <pravinkori@protonmail.ch>
2022-12-13 00:28:16 +0530

    Minor refactor in resuable card component.

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, this.cardChild});
+  ReusableCard({required this.colour, this.cardChild, this.onPress});
 
-  final Widget? cardChild;
   final Color colour;
+  final VoidCallback? onPress;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
